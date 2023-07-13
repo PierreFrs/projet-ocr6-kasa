1faf46cbc6ea389abbb081bae706f0d26bee0b26
Pierre <pierrefraisse90@gmail.com>
2023-07-13 16:58:48 +0200

    style : collapse background color

--- src/components/Collapse/index.jsx
+++ src/components/Collapse/index.jsx
@@ -14,23 +14,28 @@ const Collapse = ({value, index, toggleCollapse}) => {
 
 
   return (
+  <div className="collapse-container">
     <div className={`collapse ${value.open ? 'open' : ''}`} >
-      <h3 className='collapse-title'>
+      <h3 className="collapse-title">
         {value.title}
-        <div className='collapse-arrow' onClick={() => toggleCollapse(index)} />
+        <div className="collapse-arrow" onClick={() => toggleCollapse(index)} />
       </h3>
       <div className="collapse-paragraphe-container" style={{ maxHeight }}>
         <div ref={contentRef}>
-          {Array.isArray(value.content) ? 
-        <ul className="collapse-paragraphe">
-          {value.content.map((item, i) => <li key={i} className="equipement-item">{item}</li>)} 
-        </ul> :
-        <p className="collapse-paragraphe">{value.content}</p>
-        }
+          {Array.isArray(value.content) ? (
+            <ul className="collapse-paragraphe">
+              {value.content.map((item, i) => (
+                <li key={i} className="equipement-item">{item}</li>
+              ))}
+            </ul>
+          ) : (
+            <p className="collapse-paragraphe">{value.content}</p>
+          )}
         </div>
-        
       </div>
     </div>
+  </div>
+
   )
 };
 export default Collapse
