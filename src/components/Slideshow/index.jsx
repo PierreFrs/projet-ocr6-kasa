3501e40716c10b6015b112c7a57769cc68ac6989
Pierre <pierrefraisse90@gmail.com>
2023-07-13 17:58:58 +0200

    component : slideshow

--- src/components/Slideshow/index.jsx
+++ src/components/Slideshow/index.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useParams } from 'react-router-dom';
+import rentals from '../../data/rentals.json';
+import "./slideshow.scss"
+
+
 
 const Slideshow = () => {
-  return (
-    <div>Slideshow</div>
-  )
+
+  const { logementId } = useParams();
+  const logement = rentals.find((rental) => rental.id === logementId);
+
+  const [pictureIndex, setPictureIndex] = useState(0);
+  const [activePicture, setActivePicture] = useState(logement.pictures[0]);
+  const picturesCount = logement.pictures.length;
+  
+    const nextPicture = () => {
+    setPictureIndex((prevIndex) => (prevIndex + 1) % picturesCount);
+    setActivePicture(logement.pictures[(pictureIndex + 1) % picturesCount]);
+  };
+
+  const prevPicture = () => {
+    setPictureIndex((prevIndex) => (prevIndex - 1 + picturesCount) % picturesCount);
+    setActivePicture(logement.pictures[(pictureIndex - 1 + picturesCount) % picturesCount]);
+  };
+
+ return (
+    <div className='slideshow'>
+      
+      <div className="picture-container">
+        <img src={activePicture} alt="" className='active-picture' />
+      </div>
+      {picturesCount > 1 ? <div className='picture-count'>{pictureIndex + 1}/{picturesCount}</div> : null}
+      {picturesCount > 1 ? <div className='arrow-container arrow-container-previous' onClick={prevPicture}></div> : null}
+      {picturesCount > 1 ? <div className='arrow-container arrow-container-next' onClick={nextPicture}></div> : null}
+    </div>
+  );
 }
 
 export default Slideshow
